5aec92996fb53002913d5d349a8e2363e1bca91e
Timwi <github@timwi.de>
2022-09-14 14:11:44 +0200

    Update Assembly Info

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RT.Coordinates")]
+[assembly: AssemblyDescription("A tiny library with structs to represent tiles in rectilinear and hexagonal grids. Useful for 2D games and puzzles.")]
+[assembly: AssemblyProduct("RT.Coordinates")]
+[assembly: AssemblyCopyright("Copyright © Timwi 2022")]
+[assembly: ComVisible(false)]
+[assembly: Guid("80e00597-bd8d-4c94-b112-56a09d1c0d62")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
